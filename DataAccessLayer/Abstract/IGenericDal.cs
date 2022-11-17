45c91bbc6148a7f5dfd0627c8a427c959302cb1e
kubrakll <kubraklc149@gmail.com>
2022-11-17 16:11:49 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T>
+    {
+
+        //neden genericDal kullandık?
+        //Birden fazla entitilerimizin CRUD işlemlerini tek tek yazmamak adına bir çatıda yazıyoruz 
+
+        void Insert(T t); //Ekleme
+
+        void Delete(T t); //Silme
+
+        void Update(T t); //Güncelleme
+
+        List<T> GetList(); //Listeleme
+
+    }
+}
---
+++ EntityLayer/Concrete/Feature2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature2
+    {
+        [Key]
+        public int Feature2ID { get; set; }
+
+        public string Posts1Name { get; set; }
+
+        public string Posts1Description { get; set; }
+
+        public string Posts1Image { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
